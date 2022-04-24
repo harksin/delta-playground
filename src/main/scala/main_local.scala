bafdeb6394ca7a7cf0fb403db0e0e645c0891e6e
harksin <r.dardie@gmail.com>
2022-04-24 17:16:20 +0200

    clean :recycle:
    .

--- src/main/scala/main_local.scala
+++ src/main/scala/main_local.scala
@@ -2,15 +2,14 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.types._
 
-
 object main_local extends App {
 
   val rootLogger = Logger.getRootLogger()
   rootLogger.setLevel(Level.ERROR)
   rootLogger.setLevel(Level.INFO)
 
-
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("Delta stream s3")
     .master("local[*]")
     .config("spark.executor.instances", "1")
@@ -18,33 +17,27 @@ object main_local extends App {
     .config("spark.sql.debug.maxToStringFields", "1000000")
     .getOrCreate()
 
-  spark.sparkContext.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-  spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "http://127.0.0.1:9000");
-  spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", "minio_access_key");
-  spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", "minio_secret_key");
-  spark.sparkContext.hadoopConfiguration.set("fs.s3a.path.style.access", "true")
-
-
-  val data = Seq(("James ","Rose","Smith","36636","M",3000),
-    ("Michael","Rose","","40288","M",4000),
-    ("Robert","Mary","Williams","42114","M",4000),
-    ("Maria","Anne","Jones","39192","F",4000),
-    ("Jen","Mary","Brown","1234","F",-1)
+  val data = Seq(
+    ("James ", "Rose", "Smith", "36636", "M", 3000),
+    ("Michael", "Rose", "", "40288", "M", 4000),
+    ("Robert", "Mary", "Williams", "42114", "M", 4000),
+    ("Maria", "Anne", "Jones", "39192", "F", 4000),
+    ("Jen", "Mary", "Brown", "1234", "F", -1)
   )
 
-  val columns = Seq("firstname","middlename","lastname","dob","gender","salary")
+  val columns =
+    Seq("firstname", "middlename", "lastname", "dob", "gender", "salary")
   import spark.sqlContext.implicits._
-  val df = data.toDF(columns:_*)
+  val df = data.toDF(columns: _*)
 
   df.show()
   df.printSchema()
 
   df.write
     .mode(SaveMode.Overwrite)
-    .parquet("./people.parquet")
+    .parquet("./local-data/people-raw.parquet")
 
-
-  val parqDF = spark.read.parquet("./people.parquet")
+  val parqDF = spark.read.parquet("./local-data/people-raw.parquet")
   parqDF.createOrReplaceTempView("ParquetTable")
 
   spark.sql("select * from ParquetTable where salary >= 4000").explain()
@@ -55,12 +48,9 @@ object main_local extends App {
 
   df.write
     .mode(SaveMode.Overwrite)
-    .partitionBy("gender","salary")
-    .parquet("./people2.parquet")
+    .partitionBy("gender", "salary")
+    .parquet("./local-data/people-partitioned.parquet")
 
   spark.stop()
 
-
-
 }
-
--- src/main/scala/main_minio.scala
+++ src/main/scala/main_minio.scala
@@ -1,15 +1,14 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
-
 object main_minio extends App {
 
   val rootLogger = Logger.getRootLogger()
   rootLogger.setLevel(Level.ERROR)
   rootLogger.setLevel(Level.INFO)
 
-
-  val spark = SparkSession.builder()
+  val spark = SparkSession
+    .builder()
     .appName("Delta stream s3")
     .master("local[*]")
     .config("spark.executor.instances", "1")
@@ -17,23 +16,28 @@ object main_minio extends App {
     .config("spark.sql.debug.maxToStringFields", "1000000")
     .getOrCreate()
 
-  spark.sparkContext.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-  spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "http://127.0.0.1:9000")
-  spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", sys.env.get("MINIO_ACCES_KEY").get)
-  spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", sys.env.get("MINIO_SECRET_KEY").get)
+  spark.sparkContext.hadoopConfiguration
+    .set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+  spark.sparkContext.hadoopConfiguration
+    .set("fs.s3a.endpoint", "http://127.0.0.1:9000")
+  spark.sparkContext.hadoopConfiguration
+    .set("fs.s3a.access.key", sys.env.get("MINIO_ACCES_KEY").get)
+  spark.sparkContext.hadoopConfiguration
+    .set("fs.s3a.secret.key", sys.env.get("MINIO_SECRET_KEY").get)
   spark.sparkContext.hadoopConfiguration.set("fs.s3a.path.style.access", "true")
 
-
-  val data = Seq(("James ","Rose","Smith","36636","M",3000),
-    ("Michael","Rose","","40288","M",4000),
-    ("Robert","Mary","Williams","42114","M",4000),
-    ("Maria","Anne","Jones","39192","F",4000),
-    ("Jen","Mary","Brown","1234","F",-1)
+  val data = Seq(
+    ("James ", "Rose", "Smith", "36636", "M", 3000),
+    ("Michael", "Rose", "", "40288", "M", 4000),
+    ("Robert", "Mary", "Williams", "42114", "M", 4000),
+    ("Maria", "Anne", "Jones", "39192", "F", 4000),
+    ("Jen", "Mary", "Brown", "1234", "F", -1)
   )
 
-  val columns = Seq("firstname","middlename","lastname","dob","gender","salary")
+  val columns =
+    Seq("firstname", "middlename", "lastname", "dob", "gender", "salary")
   import spark.sqlContext.implicits._
-  val df = data.toDF(columns:_*)
+  val df = data.toDF(columns: _*)
 
   df.show()
   df.printSchema()
@@ -41,7 +45,6 @@ object main_minio extends App {
   df.write
     .mode(SaveMode.Overwrite)
     .parquet("s3a://lake-0/people.parquet")
-
 
   val parqDF = spark.read.parquet("s3a://lake-0/people.parquet")
   parqDF.createOrReplaceTempView("ParquetTable")
@@ -54,12 +57,9 @@ object main_minio extends App {
 
   df.write
     .mode(SaveMode.Overwrite)
-    .partitionBy("gender","salary")
+    .partitionBy("gender", "salary")
     .parquet("s3a://lake-0/people2.parquet")
 
   spark.stop()
 
-
-
 }
-
